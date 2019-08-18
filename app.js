dba63ce29c843c1ca625b1f208dcc912b1c76112
Piotr Chojnacki <chojnacki.piotr91@gmail.com>
2019-08-18 23:12:01 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,309 @@
+console.log('%c- Task 1 -', 'padding:5px; color:white; background-color:navy; font-size:16px')
+
+String.prototype.toJadenCase = function () { 
+    return this.split(' ').map((word)=>{
+        return word.charAt(0).toUpperCase()+word.slice(1)
+    }).join(' ')
+  }
+
+  /*Komentarz
+  
+    1. String.prototype --> Reprezentuje prototyp obiektów danej klasy. Pozwala na dodawanie własności i metod do wszystkich instancji tej klasy.
+    
+    >>>> Dzięki takiemu zapisowi w funkcji mogę skorzystać z >>this<< bez przekazywania argumentu do funkcji.
+
+    >>>> Taki zapis pozwala również na skorzystanie z wszystkich metod, które dotyczą stringów.
+
+    2. this.split(' ')
+
+    >>>> string.split(separator, limit) 
+    Metoda JS pozwalająca na zmianę stringa w tablicę.
+    !!! Metoda ZWRACA tablicę !!!
+    > separator - pozwala na określenie w jaki znak ma rodzielać stringa. Kiedy parser natrafia na znak określony w separatorze wrzuca dany tekst przed nim jako jeden z elementów tablicy.
+
+    >limit - określa ile razy parser ma dokonać rozdzielenia. Oznacza to, że po wykonaniu dwóch cięć zwracana jest tablica z dwoma stringami.
+
+    3 .map((word)=>{})
+
+    >>> .map() to funkcja wyższego rzędu, która jako argument przyjmuje inną funkcję. 
+    !!! Metoda ZWRACA tablicę, która jest wynikiem określonego w funkcji /callback/ działania na każdym elemencie przekazanej tablicy.
+
+    var new_array = arr.map(function callback(currentValue, index, array){}
+
+    > currentValue - element z przekazywanej tablicy w tym przypadku arr
+
+    > index - index z przekazywanej tablicy w tym przypadku arr[i]
+
+    > array - oryginalna tablica na rzecz której wywołano metodę map
+
+    4. return word.charAt(0).toUpperCase() + word.slice(1);
+
+    return - funkcje zazwyczaj coś zwracają
+
+    word.charAt(0) - metoda dedykowana stringą zwraca znak znajdujący się na wskazanej pozycji
+
+    .toUpperCase() - metoda dedykowana stringą, zmienia znak z małej na WIELKĄ literę
+
+    word.slice(1) - metoda dedykowana stringą, wycina określoną część stringa.
+    string.slice(start, end) - pozostawienie drugiego parametru bez niczego powoduje, że wycinany jest od indeksu określonego w start do końca danego stringa.
+    Pominięcie obydwu parametrów powoduje, że cały string pojawia się tak jak został przekazany.
+
+    >>> DZIAŁANIE tego zapisu:
+    >>>> return word.charAt(0).toUpperCase() + word.slice(1); 
+
+    > Zwróc pierwszą literę przekazywanego elementu --> zrób z tej litery Wielką literę ---> połącz ją z przekazywanym elementem ale z częścią od drugiej litery do końca tego stringa.
+
+    5. .join(' ');
+
+    array.join(separator)
+
+    >>> metoda dedykowana TABLICĄ, która zmiania array na stringa. 
+    MAP zwróciło nam tablicę a my chcemy stringa stąd zastosowanie metody tablicowej mimo tego, że do tej pory korzystaliśmy z metod dla stringów.
+
+    >>> podobnie jak w split w join korzystamy z separatora, który rozdzieli w tablicy każdy element i pokaże w stringu.
+
+  */
+
+var str = "How can mirrors be real if our eyes aren't real";
+console.log(str);
+console.log('%c- WYNIK -','padding:5px; color:white; background-color:black; font-size: 16px');
+console.log(str.toJadenCase());
+
+console.log('%c- Task 2 -', 'padding:5px; color:white; background-color:navy; font-size:16px')
+
+function isIsogram(str) {
+  let newStr = str.toLowerCase();
+  if (newStr.length===0){
+    return true
+  } else {
+    let arr = newStr.split('').sort();
+    let testArr = [];
+    for (let i=0; i<arr.length; i++){
+        if(arr[i]===arr[i+1]){
+          testArr.push(false)
+        } else {
+          testArr.push(true)
+        }
+    }
+    if (testArr.indexOf(false)>=0){
+      return false
+    } else {
+      return true
+    }
+  }
+}
+
+console.log('%c- WYNIK -','padding:5px; color:white; background-color:black; font-size: 16px');
+console.log(isIsogram("Dermatoglyphics"))
+console.log(isIsogram("aba"))
+console.log(isIsogram( "mOose" ))
+
+console.log('%c- Task 2 z wykorzystaniem set -', 'padding:5px; color:white; background-color:navy; font-size:16px')
+
+function isIsogram2 (str) {
+  return !str || (str.length === new Set(str.toLowerCase()).size) 
+}
+
+console.log('%c- WYNIK -','padding:5px; color:white; background-color:black; font-size: 16px');
+console.log(isIsogram2("Dermatoglyphics"))
+console.log(isIsogram2("aba"))
+console.log(isIsogram2( "mOose" ))
+
+console.log('%c- Task 3 z wykorzystaniem set -', 'padding:5px; color:white; background-color:navy; font-size:16px')
+
+function isPangram(str){
+  return !str || new Set(str.toLowerCase().replace(/ /g,'').replace('.','')).size === 26
+}
+
+console.log('%c- WYNIK -','padding:5px; color:white; background-color:black; font-size: 16px');
+console.log(isPangram("The quick brown fox jumps over the lazy dog."))
+
+console.log('%c- Task 4 -', 'padding:5px; color:white; background-color:navy; font-size:16px');
+
+function alphabetPosition(text) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz",
+      arr = [],
+      splitedString = text.toLowerCase().replace(/[^a-zA-Z]/g, "").split('');
+      splitedString.forEach(element => {
+          arr.push((alphabet.indexOf(element))+1)
+      });
+      return arr.toString().replace(/,/g,' ');
+}
+
+console.log('%c- WYNIK -','padding:5px; color:white; background-color:black; font-size: 16px');
+console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+
+console.log('%c- Task 5 -', 'padding:5px; color:white; background-color:navy; font-size:16px');
+
+function longestConsec(strarr, k) {
+  let str = '';
+  strarr.sort((a,b)=>{return b.length - a.length});
+  if (strarr.length===0||k>strarr.length||k<=0){
+    return str;
+  } else {
+    for (let i=0; i<k; i++){
+      str += strarr[i]
+    }
+    return str;
+  }
+}
+
+// https://www.codewars.com/kata/consecutive-strings/train/javascript
+
+function longestConsec2(strarr, k) {
+  var n = strarr.length, 
+      str = '';
+
+  if (n = 0 || k > n || k <= 0) {
+      return str;
+  }
+
+  for (var i = 0; i < strarr.length; i++) {
+      var currentStr = strarr.slice(i, k + i).join('');
+      if (currentStr.length > str.length) {
+          str = currentStr;
+      }
+  }
+  return str;
+}
+
+console.log('%c- WYNIK -','padding:5px; color:white; background-color:black; font-size: 16px');
+console.log(longestConsec2(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2));
+console.log('wlwsasphmxxowiaxujylentrklctozmymu');
+console.log(longestConsec2(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2))
+
+console.log('%c- Task 6 -', 'padding:5px; color:white; background-color:navy; font-size:16px');
+
+String.prototype.camelCase=function(){
+  let textArr = this.trim().split('');
+  let camelCase = "";
+  for (var i=0; i<textArr.length; i++){
+    if (i===0){
+      let firstLetter = textArr[i].toUpperCase();
+      camelCase+=firstLetter
+    } else if (textArr[i]===" "){
+      let capitalLetter = textArr[i+1].toUpperCase();
+      camelCase+=capitalLetter;
+      i++
+    } else {
+      camelCase+=textArr[i];
+    }
+  }
+  return camelCase
+}
+
+"hello case".camelCase()
+"camel case word".camelCase()
+" camel case word".camelCase()
+
+String.prototype.camelCase2=function(){
+  return this.split(' ').map(function(word){
+   return word.charAt(0).toUpperCase() + word.slice(1);
+ }).join('');
+}
+
+
+console.log("hello case".camelCase2())
+console.log("camel case word".camelCase2())
+console.log(" camel case word".camelCase2())
+
+console.log('%c- Task 7 -', 'padding:5px; color:white; background-color:navy; font-size:16px');
+// Senior - at least 55 and handicap greater than 7
+// 
+
+openOrSenior = (data) =>{
+  let test =  data.map(element => {
+    if(element[0]>=55&&element[1]>7){
+      return "Senior"
+    } else {
+      return "Open"
+    }
+  })
+  return test;
+}
+
+// Destructuring: [age, handicap] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+// Arrow Functions: () => {} https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+function openOrSenior2(data){
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+}
+
+console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]));
+
+console.log('%c- Task 8 -', 'padding:5px; color:white; background-color:navy; font-size:16px');
+
+function iqTest(numbers){
+  // zmiana stringa w tablicę liczb
+  numbers = numbers.split(" ").map(function(el){return parseInt(el)});
+  
+  var odd = numbers.filter(function(el){ return el % 2 === 1});
+  var even = numbers.filter(function(el){ return el % 2 === 0});
+  console.log(odd,even)
+  
+  return odd.length < even.length ? (numbers.indexOf(odd[0]) + 1) : (numbers.indexOf(even[0]) + 1);
+}
+console.log('%c- Task 9 Persistent Bugger -', 'padding:5px; color:white; background-color:navy; font-size:16px');
+function persistence(num) {
+  var times = 0;
+  
+  num = num.toString();
+  
+  while (num.length > 1) {
+    times++;
+    num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+  }
+  
+  return times;
+}
+
+console.log(persistence(39));
+console.log(persistence(4));
+console.log(persistence(25));
+console.log(persistence(999));
+
+console.log('%c- Task 10 Beginner Series #3 Sum of Numbers -', 'padding:5px; color:white; background-color:navy; font-size:16px');
+
+GetSum = (a,b) => {
+  let numbers=[]
+
+  if (a===b){
+    return a
+  } else if (a>b) {
+    for (let i=a; i>(b-1); i--){
+      numbers = [...numbers, i];
+    }
+    return numbers.reduce((p,c)=>p+c)
+  } else {
+    for (let i=a; i<(b+1); i++){
+      numbers = [...numbers, i];
+    }
+    return numbers.reduce((p,c)=>p+c)
+  }
+}
+
+function GetSum2(a, b) {
+  return (a + b) * (Math.abs(a - b) + 1) / 2;
+}
+
+console.log(GetSum(0, -1));
+console.log(GetSum(0, 1));
+console.log(GetSum(1, 1));
+console.log(GetSum2(0, -1));
+console.log(GetSum2(0, 1));
+console.log(GetSum2(1, 1));
+
+console.log('%c- Task 11 Sum of odd numbers -', 'padding:5px; color:white; background-color:navy; font-size:16px');
+
+function rowSumOddNumbers(n) {
+  let start = Math.pow(n,2)-(n-1);
+  let numbers = [start];
+  while (numbers.length<n){
+    start++
+    if(start%2!==0){
+      numbers.push(start)
+    }
+  }
+  return numbers.reduce((p,c)=>p+c);
+}
+console.log(rowSumOddNumbers(20));
---
+++ readMe.md
@@ -0,0 +1,148 @@
+# Zadania z **Code Wars** z moimi komentarzami
+
+## Task1
+
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word.
+
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+```
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased: "How Can Mirrors Be Real If Our Eyes Aren't Real"
+```
+
+## Task2
+
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+```
+isIsogram( "Dermatoglyphics" ) == true
+isIsogram( "aba" ) == false
+isIsogram( "moOse" ) == false // -- ignore letter case
+```
+
+## Task3
+
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence ```"The quick brown fox jumps over the lazy dog"``` is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+## Task4
+
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+```
+"a" = 1, "b" = 2, etc.
+```
+```
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+```
+
+## Task5
+
+You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+```Example:```
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+
+## Task6
+
+Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+For instance:
+```
+"hello case".camelCase() => HelloCase
+"camel case word".camelCase() => CamelCaseWord
+```
+
+## Task7
+
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input
+Input will consist of a list of lists containing two items each. Each list contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Note for F#: The input will be of (int list list) which is a List<List>
+
+```Example Input```
+
+```
+[[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
+```
+Output
+Output will consist of a list of string values (in Haskell: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+```Example Output```
+
+```
+["Open", "Open", "Senior", "Open", "Open", "Senior"]
+```
+## Task8
+
+Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+
+! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+
+##Examples :
+````
+iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
+
+iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd
+````
+
+## Task9
+
+Write a function, ``persistence``, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example:
+````
+ persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+                       // and 4 has only one digit
+
+ persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
+                        // 1*2*6 = 12, and finally 1*2 = 2
+
+ persistence(4) === 0 // because 4 is already a one-digit number
+ ````
+## Task10
+
+Given two integers a and b, which can be positive or negative, find the sum of all the numbers between including them too and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples
+````
+GetSum(1, 0) == 1   // 1 + 0 = 1
+GetSum(1, 2) == 3   // 1 + 2 = 3
+GetSum(0, 1) == 1   // 0 + 1 = 1
+GetSum(1, 1) == 1   // 1 Since both are same
+GetSum(-1, 0) == -1 // -1 + 0 = -1
+GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+````
+
+## Task11
+
+Given the triangle of consecutive odd numbers:
+````
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+````
+
+Calculate the row sums of this triangle from the row index (starting at index 1) e.g.:
+````
+rowSumOddNumbers(1); // 1
+rowSumOddNumbers(2); // 3 + 5 = 8
+````
